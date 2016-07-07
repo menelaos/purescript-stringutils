51f918e3eb353f6a0f2d1c358aa9d32a93ba3f69
Andreas Schacker <menelaos@users.noreply.github.com>
2016-07-07 20:28:21 +0200

    Add `filter` function

--- src/Data/String/Utils.purs
+++ src/Data/String/Utils.purs
@@ -1,10 +1,14 @@
 module Data.String.Utils
   ( codePointAt
+  , filter
   )
 where
 
 import Data.Maybe        (Maybe(Just, Nothing))
+import Data.String       (fromCharArray, toCharArray)
 import Prelude
+
+import Data.Array as Array
 
 -- | Returns the Unicode code point value of the character at the given index,
 -- | if the index is within bounds.
@@ -17,3 +21,7 @@ foreign import _codePointAt
   -> Int
   -> String
   -> Maybe Int
+
+-- | Keep only those characters that satisfy the predicate.
+filter :: (Char -> Boolean) -> String -> String
+filter p = fromCharArray <<< Array.filter p <<< toCharArray
--- test/Test/Data/String/Utils.purs
+++ test/Test/Data/String/Utils.purs
@@ -4,9 +4,9 @@ where
 
 import Control.Monad.Eff.Console (log)
 import Data.Maybe                (Maybe (Just, Nothing))
-import Data.String.Utils         (codePointAt)
+import Data.String.Utils         (codePointAt, filter)
 import Prelude
-import Test.StrongCheck          (SC, (===), assert)
+import Test.StrongCheck          (Result, SC, (===), assert, quickCheck)
 
 testStringUtils :: SC () Unit
 testStringUtils = do
@@ -19,3 +19,27 @@ testStringUtils = do
   assert $ codePointAt 2 "ab" === Nothing
   assert $ codePointAt 0 "∀"  === Just 8704
   assert $ codePointAt 1 "∀ε" === Just 949
+
+  log "filter"
+  let
+    filterIdProp :: String -> Result
+    filterIdProp str = filter (const true) str === str
+
+    filterNukeProp :: String -> Result
+    filterNukeProp str = filter (const false) str === ""
+
+    filterIdempotenceProp :: (Char -> Boolean) -> String -> Result
+    filterIdempotenceProp f str = filter f (filter f str) === filter f str
+
+    filterDistributiveProp :: (Char -> Boolean) -> String -> String -> Result
+    filterDistributiveProp f a b =
+      filter f (a <> b) === filter f a <> filter f b
+
+    filterEmptyStringProp :: (Char -> Boolean) -> Result
+    filterEmptyStringProp f = filter f "" === ""
+
+  quickCheck filterIdProp
+  quickCheck filterIdempotenceProp
+  quickCheck filterDistributiveProp
+  quickCheck filterEmptyStringProp
+  quickCheck filterNukeProp
