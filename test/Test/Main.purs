bd112fd663ca93ad09826b3708c32d49438b9dda
Maxim Zimaliev <zimaliev@gmail.com>
2018-05-10 16:09:48 +0300

    updates for 0.12-rc

--- test/Test/Main.purs
+++ test/Test/Main.purs
@@ -1,9 +1,12 @@
 module Test.Main where
 
 import Prelude
+
+import Effect (Effect)
 import Test.Data.Char.Utils   (testCharUtils)
 import Test.Data.String.Utils (testStringUtils)
 
+main :: Effect Unit
 main = do
   testCharUtils
   testStringUtils
