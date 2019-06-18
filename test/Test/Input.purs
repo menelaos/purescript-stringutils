0bffb12de0316343f9f7a7b5ae6ba20b9530f035
Andreas Schacker <menelaos@users.noreply.github.com>
2019-06-18 20:06:46 +0200

    Update for PureScript 0.13.0

--- test/Test/Input.purs
+++ test/Test/Input.purs
@@ -49,12 +49,11 @@ instance arbOneCharString :: Arbitrary OneCharString where
 
 newlineChars :: List Char
 newlineChars = fromFoldable
-  [ '\n', '\v', '\f', '\r', '\x0085', '\x2028', '\x2029' ]
+  [ '\n', '\r', '\x0085', '\x2028', '\x2029' ]
 
 whiteSpaceChars :: List Char
 whiteSpaceChars = fromFoldable
-  [ ' ', '\f', '\n', '\r', '\t', '\v', '\x00A0', '\x1680', '\x2000', '\x2001'
-  , '\x2002', '\x2003', '\x2004', '\x2005', '\x2006', '\x2007', '\x2008'
-  , '\x2009', '\x200A', '\x2028', '\x2029', '\x202F', '\x205F', '\x3000'
-  , '\xFEFF'
+  [ ' ', '\n', '\r', '\t', '\x00A0', '\x1680', '\x2000', '\x2001', '\x2002'
+  , '\x2003', '\x2004', '\x2005', '\x2006', '\x2007', '\x2008', '\x2009'
+  , '\x200A', '\x2028', '\x2029', '\x202F', '\x205F', '\x3000', '\xFEFF'
   ]
